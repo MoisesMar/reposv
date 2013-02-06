f109cae7db6a89513a7db90b1c45151c8c3809f4
CirSispra01 <cirsispra01@cirsispra01.(none)>
2013-02-06 13:36:30 -0500

    Se agrupa carpeta

---
+++ Reposv/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba3::Application.config.secret_token = '5588d2b9e7987befc98b795c168c19be9b2e0089b24916c303f861228141c4542648f7360cb50e28d2cc1c478594c4d9942a7d00d1526e5b1286c1a8bb85f5d0'
